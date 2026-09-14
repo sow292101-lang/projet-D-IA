a5fedaaab3f600786c17ccd3a2c9647738d9a1ff
Mohamed Sow <Sow292101@gmail.com>
2026-09-14 16:16:22 +0200

    premier commit teste

---
+++ src/First/Firstapp.java
@@ -0,0 +1,21 @@
+package First;
+
+
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.utility.Delay;
+
+public class Firstapp {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
+
+        g.drawString("Hello World", 0, 0, GraphicsLCD.VCENTER | GraphicsLCD.LEFT);
+
+        Delay.msDelay(5000);
+    }
+
+}
